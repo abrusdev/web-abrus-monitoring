f3aa344c20f03f66943d2587af9fdb1feacea9e0
abrusdev <abrusdev@gmail.com>
2023-02-17 01:32:27 +0500

    add: card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import HomePage from "./pages/home/HomePage";
+import AppContext from "./context/AppContext";
 
 function App() {
+
+  const colors = {
+    primaryColor: "#181D28",
+    secondaryColor: "#CBE7EF",
+    whiteColor: "#FDFFFF",
+    blackColor: "#212121"
+  }
+
   return (
-    <>
+    <AppContext.Provider value={{
+      colors,
+      budget: 225000
+    }}>
       <HomePage />
-    </>
+    </AppContext.Provider>
   )
 }
 
--- src/pages/home/components/card/HomeCard.jsx
+++ src/pages/home/components/card/HomeCard.jsx
@@ -1,21 +1,81 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { useContext } from "react";
+import AppContext from "../../../../context/AppContext";
 
 
 const useStyles = makeStyles(() => ({
   container: {
-    width: 400,
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    width: 350,
     height: 200,
-    background: '#181D28',
-    borderRadius: 8
+    borderRadius: 8,
+    overflow: 'hidden',
+    padding: 20,
+    background: ({ colors }) => colors.primaryColor,
+    boxShadow: ({ colors }) => `1px 1px 10px 2px ${colors.primaryColor}20`
+  },
+  label: {
+    fontSize: 12,
+    fontWeight: 400,
+    color: ({ colors }) => `${colors.whiteColor}60`,
+  },
+  value: {
+    marginTop: 20,
+    fontSize: 32,
+    fontWeight: 500,
+    color: ({ colors }) => colors.whiteColor,
+  },
+  bottomContainer: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+
+    display: 'flex',
+    columnGap: 40,
+    padding: '12px 20px',
+
+    width: '100%',
+    background: ({ colors }) => colors.secondaryColor,
+  },
+  bottomItem: {
+    display: "flex",
+    flexDirection: 'column'
+  },
+  bottomLabel: {
+    fontSize: 12,
+    fontWeight: 400,
+    opacity: 0.6,
+    color: ({ colors }) => colors.blackColor,
+  },
+  bottomValue: {
+    fontSize: 14,
+    fontWeight: 700,
+    color: ({ colors }) => colors.blackColor,
   }
 }))
 
 function HomeCard() {
-  const styles = useStyles();
+  const { colors, budget } = useContext(AppContext);
+
+  const styles = useStyles({ colors });
 
   return (
     <div className={styles.container}>
-      <h2>Budget</h2>
+      <p className={styles.label}>Ahli Kvartira</p>
+      <h3 className={styles.value}>${budget}</h3>
+
+      <div className={styles.bottomContainer}>
+        <div className={styles.bottomItem}>
+          <p className={styles.bottomLabel}>Members</p>
+          <span className={styles.bottomValue}>7</span>
+        </div>
+        <div className={styles.bottomItem}>
+          <p className={styles.bottomLabel}>Updated</p>
+          <span className={styles.bottomValue}>3 hours ago</span>
+        </div>
+      </div>
     </div>
   )
 }
